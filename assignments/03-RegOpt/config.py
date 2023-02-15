9f55a051a48fbedaaad383feb885bacaf504bde0
Kaya Panchalingam <kayapanchalingam@Kayas-MacBook-Pro.local>
2023-02-15 11:19:40 -0500

    w/ norm

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -32,9 +32,4 @@ class CONFIG:
         weight_decay=CONFIG.initial_weight_decay,
     )
 
-    transforms = Compose(
-        [
-            ToTensor(),
-            Normalize(0,1)
-        ]
-    )
+    transforms = Compose([ToTensor(), Normalize(0, 1)])
