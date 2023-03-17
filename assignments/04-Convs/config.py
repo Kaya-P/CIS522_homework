a5ccda7434e5b83ae74d76b7ae912ea2f000e33c
Kaya Panchalingam <kayapanchalingam@Kayas-MacBook-Pro.local>
2023-03-16 22:03:23 -0400

    try3

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,7 +2,9 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor
+
+# from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
@@ -17,4 +19,6 @@ class CONFIG:
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    transforms = Compose([ToTensor()])
+    transforms = Compose(
+        [ToTensor(), Normalize(mean=[0.485, 0.456, 0.4], std=[0.229, 0.224, 0.2])]
+    )
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -16,8 +16,8 @@ class Model(torch.nn.Module):
         self.conv1 = torch.nn.Conv2d(num_channels, 6, 5)
         self.pool = torch.nn.MaxPool2d(2, 2)
         self.conv2 = torch.nn.Conv2d(6, 16, 5)
-        self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = torch.nn.Linear(120, 84)
+        # self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = torch.nn.Linear(16 * 5 * 5, 84)
         self.fc3 = torch.nn.Linear(84, num_classes)
 
         # 3,chaneel, 10 classes
@@ -29,7 +29,7 @@ class Model(torch.nn.Module):
         x = self.pool(torch.nn.functional.relu(self.conv1(x)))
         x = self.pool(torch.nn.functional.relu(self.conv2(x)))
         x = x.view(-1, 16 * 5 * 5)
-        x = torch.nn.functional.relu(self.fc1(x))
+        # x = torch.nn.functional.relu(self.fc1(x))
         x = torch.nn.functional.relu(self.fc2(x))
         x = self.fc3(x)
         return x
